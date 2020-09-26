b00eafdfd093ac54ec9f84a21c87ef9d6c05f422
Marina Savitzki <marinasavitzki@gmail.com>
2020-09-25 22:01:34 -0300

    Criando rotas

--- src/App.js
+++ src/App.js
@@ -1,8 +1,42 @@
 import React from 'react';
+/*
+BrowserRouter - roteador do navegador
+Switch - faz a alternancia entre as rotas
+Route -criar uma rota 
+Link - criar um link
+*/
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 
+import Home from './screens/Home'
+import Detail from './screens/Detail'
 //Aplicacao
 const App = () => {
-    return <h1 > App < /h1>;
+    return (
+        <BrowserRouter>
+            <ul>
+                <li>
+                    <Link to='/'>Home</Link>
+                </li>
+                <li>
+                    <Link to='/Detail'>Detail</Link>
+                </li>
+
+            </ul>
+
+            <Switch>
+                {/*Passando a rota da pagina detail */}
+                <Route path='/detail'>
+                    < Detail />
+                </Route>
+                {/*Passando a rota da pagina home */}
+                {/*É importante que esta rota fique por ultimo por como é a raiz, se ficar acima das outras pode pegar uma rota errada quando
+                for redirecionada */}
+                <Route path='/'>
+                    <Home />
+                </Route>
+            </Switch>
+        </BrowserRouter>
+    );
 
 };
 
--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,6 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { doGet } from '../../helpers/ApiHelper';
+
 
 const Home = () => {
+
+    const [people, setpeople] = useState([]);
+
+    //Executa apenas uma vez
+    useEffect(() => {
+        doGet('/people').then(response => setpeople(response.results));
+
+    });
+
+
+    console.log(people);
 
     return <h1 > Home </h1>;
 };
